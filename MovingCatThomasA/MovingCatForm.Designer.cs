c13b475ab1effe6f6f1eb7177c5d60408a2910b2
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-04-05 20:58:04 -0400

    Final

--- MovingCatThomasA/MovingCatForm.Designer.cs
+++ MovingCatThomasA/MovingCatForm.Designer.cs
@@ -31,13 +31,19 @@
             this.mnuMainMenu = new System.Windows.Forms.MenuStrip();
             this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
             this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.picCat = new System.Windows.Forms.PictureBox();
             this.mnuMainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).BeginInit();
             this.SuspendLayout();
             // 
             // mnuMainMenu
             // 
             this.mnuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mniFile});
+            this.mniFile,
+            this.mniCat});
             this.mnuMainMenu.Location = new System.Drawing.Point(0, 0);
             this.mnuMainMenu.Name = "mnuMainMenu";
             this.mnuMainMenu.Size = new System.Drawing.Size(284, 24);
@@ -54,21 +60,56 @@
             // mniExit
             // 
             this.mniExit.Name = "mniExit";
-            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
             this.mniExit.Text = "Exit";
             this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // mniCat
+            // 
+            this.mniCat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniCat1,
+            this.mniCat2});
+            this.mniCat.Name = "mniCat";
+            this.mniCat.Size = new System.Drawing.Size(37, 20);
+            this.mniCat.Text = "Cat";
+            // 
+            // mniCat1
+            // 
+            this.mniCat1.Name = "mniCat1";
+            this.mniCat1.Size = new System.Drawing.Size(152, 22);
+            this.mniCat1.Text = "Cat1";
+            this.mniCat1.Click += new System.EventHandler(this.mniCat1_Click);
+            // 
+            // mniCat2
+            // 
+            this.mniCat2.Name = "mniCat2";
+            this.mniCat2.Size = new System.Drawing.Size(152, 22);
+            this.mniCat2.Text = "Cat2";
+            this.mniCat2.Click += new System.EventHandler(this.mniCat2_Click);
+            // 
+            // picCat
+            // 
+            this.picCat.Image = global::MovingCatThomasA.Properties.Resources.cat1;
+            this.picCat.Location = new System.Drawing.Point(77, 87);
+            this.picCat.Name = "picCat";
+            this.picCat.Size = new System.Drawing.Size(109, 126);
+            this.picCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCat.TabIndex = 1;
+            this.picCat.TabStop = false;
             // 
             // frmMovingCat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.picCat);
             this.Controls.Add(this.mnuMainMenu);
             this.MainMenuStrip = this.mnuMainMenu;
             this.Name = "frmMovingCat";
             this.Text = "Moving Cat By Thomas Aubin";
             this.mnuMainMenu.ResumeLayout(false);
             this.mnuMainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +120,10 @@
         private System.Windows.Forms.MenuStrip mnuMainMenu;
         private System.Windows.Forms.ToolStripMenuItem mniFile;
         private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem mniCat;
+        private System.Windows.Forms.ToolStripMenuItem mniCat1;
+        private System.Windows.Forms.ToolStripMenuItem mniCat2;
+        private System.Windows.Forms.PictureBox picCat;
     }
 }
 
--- MovingCatThomasA/MovingCatForm.cs
+++ MovingCatThomasA/MovingCatForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Thomas Aubin
+ * Created on: 5 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #8 - Moving Cat
+ * This program displays a menu bar with menu items that when clicked on will change the picture in the picture box
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,10 +25,22 @@ namespace MovingCatThomasA
             InitializeComponent();
         }
 
-        // Close the program when clicked on
         private void mniExit_Click(object sender, EventArgs e)
         {
+            // Close program when exit button clicked
             this.Close();
+        }
+
+        private void mniCat1_Click(object sender, EventArgs e)
+        {
+            // Change image to cat1 image
+            this.picCat.Image = Properties.Resources.cat1;
+        }
+
+        private void mniCat2_Click(object sender, EventArgs e)
+        {
+            // Change image to cat2 image
+            this.picCat.Image = Properties.Resources.cat2;
         }
     }
 }
